0710feaac7e53b84486b0460fe5092191a6cdb26
Shamim Imtiaz <km_si@ymail.com>
2021-01-10 18:56:34 -0500

    let user select ans, correct asn, add score

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 -hide instructions screen    
 -start timer function
 - show questions 
-let user select answer
+-let user select answer
 show correct answer
 log result for user.
 for each incorrect answer -10sec
@@ -12,19 +12,8 @@ view high scores (arrange)
 option to restart
 */
 //Questions copied from https://www.examtiger.com/mcq/css-mcq-on-stylesheet/ this is the question array with answers
-/*var questions = [
-    {q:"If we want define style for an unique element, then which css selector will we use ?",
-    ans: [{text:"Id", correct:true}, {text:"text", correct:false}, {text:"class",correct:false},{text:"name", correct:false}]}, 
-    {q:"If we don't want to allow a floating div to the left side of an element, which css property will we use ?",
-    ans: [{text:"margin",correct:false}, {text:"clear",correct:true},{text:"float",correct:false},{text:"padding", correct:false}]},
-    {q:"Suppose we want to arragnge five nos. of DIVs so that DIV4 is placed above DIV1. Now, which css property will we use to control the order of stack?",
-    ans: [{text:"d-index", correct:false}, {text:"s-index", correct:false},{text:"x-index",correct:false},{text:"z-index",correct:true}]}, 
-    {q:"Can we align a Block element by setting the left and right margins ?",
-    ans: [{text:"Yes we can",correct:false},{text:"not possible", correct:true},{text:"maybe",correct:false}, {text:"probably",correct:false}]}, 
-    {q:"If we want to wrap a block of text around an image, which css property will we use ?",
-    ans: [{text:"wrap",correct:false}, {text:"push",correct:false}, {text:"float",correct:true},{text:"align",correct:false}]}];
-*/
-
+var currentQuestionIndex = 0;
+var score=0;
 const startButton = document.getElementById("start-btn")
 const nextButton= document.getElementById("next-btn")
 const instructionsContainer = document.getElementById("instruction")
@@ -32,9 +21,13 @@ const quesContainer = document.getElementById("quesContainer")
 const questionQuizz =document.getElementById("questions")
 const ansButtonElement =document.getElementById("answer-button")
 const quizTimer = document.getElementById("timeKeeper")
+const highScores = document.getElementById("viewHighScores")
 
+var highScoreLog =[];
 startButton.addEventListener("click", hideInstructions);
+highScores.addEventListener("click", showHighScores);
 
+//Step 1 after Start button click
 function hideInstructions (){
     instructionsContainer.style.display ="none"; //make instructions dissapear
     quesContainer.style.visibility = "visible";//make question container appear
@@ -44,14 +37,15 @@ function hideInstructions (){
 }
 
     //how to decrease time by 10seconds
-    //What to do when timer is =o
     //stop timer when button "view high scores" is pressed
 function counTimer(){
+    console.log("timer function");
     var totalTime = 15;
     var id=setInterval(timerFunction, 1000);
     function timerFunction(){
         if(totalTime==0){
             clearInterval(id);
+            showHighScores();
         }else {totalTime--;
             quizTimer.textContent="Time : " +totalTime;
             }
@@ -63,43 +57,62 @@ function counTimer(){
 //LEFT TO DO : ensure we input the correct questions and answers
 
 var questions = [
-    {ques:"question 1", ans1:"ansA", ans2:"ansB", ans3:"ansC", ans4:"ansD", correct:"ansD"},
-    {ques:"question 2", ans1:"ans2A",ans2:"ans2B",ans3:"ans2C",ans4:"ans2D", correct:"ans2D"},
-    {ques:"question 3", ans1:"ans3A",ans2:"ans3B",ans3:"ans3C",ans4:"ans3D", correct:"ans3D"}];
-function quizzQuestions(){
-    console.log(questions.length);
-var i;
-for(i=0;i<questions.length; i++) {
-    questionQuizz.textContent = "Question: "+(questions[i].ques);
-    document.getElementById("ans1").textContent = questions[i].ans1;
-    document.getElementById("ans2").textContent = questions[i].ans2;
-    document.getElementById("ans3").textContent = questions[i].ans3;
-    document.getElementById("ans4").textContent = questions[i].ans4;
-    document.getElementById("ans4").textContent = questions[i].ans4;
-    document.getElementById("correctAnswer").textContent = questions[i].correct;
-   // ansButtonElement.addEventListener("onclick", whichAnswer());
-    //nextButton.addEventListener("click",alert("next button was clicked"));
+    {ques:"question 1", ans1:"ansA", ans2:"ansB", ans3:"ansC", ans4:"ansD", correct:"ans4"},
+    {ques:"question 2", ans1:"ans2A",ans2:"ans2B",ans3:"ans2C",ans4:"ans2D", correct:"ans2"},
+    {ques:"question 3", ans1:"ans3A",ans2:"ans3B",ans3:"ans3C",ans4:"ans3D", correct:"ans3"}];
+    var questionLenghtIndex=questions.length-1;
+
+    function quizzQuestions(){
+    
+    if(questionLenghtIndex===currentQuestionIndex){
+        showHighScores();
+    }else {
+        renderQuestionAns(currentQuestionIndex);  
+    }
 }
-}
+//END: How to show questions and answers
 
 
-function whichAnswer(){
-//Checking which button the user clicked and if the answer is correct. 
-if(document.getElementById("ans1").clicked) {
-    console.log("button 1 was clicked")
-}else if (document.getElementById("ans2").clicked){
-    console.log("button 2 was clicked")
-}else if (document.getElementById("ans3").clicked){
-    console.log("button 3 was clicked")
-}else {
-    console.log("button 4 was clicked")
-}
+//function to show the question and answer
+
+function renderQuestionAns(currentQuestionIndex){
+    questionQuizz.textContent = "Question: "+(questions[currentQuestionIndex].ques);
+    document.getElementById("ans1").textContent = questions[currentQuestionIndex].ans1;
+    document.getElementById("ans2").textContent = questions[currentQuestionIndex].ans2;
+    document.getElementById("ans3").textContent = questions[currentQuestionIndex].ans3;
+    document.getElementById("ans4").textContent = questions[currentQuestionIndex].ans4;
+  //  correctAnswer();
 }
 
+//END: function to show the question and answer
 
-/*<button onclick=>
+//Correct Answer checking
+function correctAnswer(answer){
+    correct = questions[currentQuestionIndex].correct;
+    console.log("the correct answer"+correct);
+    console.log("what i clicked" + answer);
+    if(answer === correct && currentQuestionIndex !== questionLenghtIndex){
+        alert("Correct Answer");
+        score++;
+        currentQuestionIndex++;
+        renderQuestionAns(currentQuestionIndex);
+    } else if (answer !== correct && currentQuestionIndex !== questionLenghtIndex){
+        alert("wrong answer");
+        currentQuestionIndex++;
+        renderQuestionAns(currentQuestionIndex);
+    }else{
+        console.log("loop completed");
+        console.log(score);
+    }
+};
 
-btn.target.matches("ansD")
-if (answer=ansD) {
-    show correct;
-} else*/
+//view high scores (arrange)
+function showHighScores(){
+    instructionsContainer.style.display ="none"; //make instructions dissapear
+    quesContainer.style.display = "none";//make question container appear  
+    nextButton.style.display="none";//hide the start button
+    startButton.textContent = "RESTART";
+    console.log("show high scores");
+    startButton.addEventListener("click", hideInstructions);
+    var hgScore = document.createElement("p"); 
+}
