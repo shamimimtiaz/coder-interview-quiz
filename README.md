3f9e54561363681b3674014f37f27f127491f88e
Shamim Imtiaz <km_si@ymail.com>
2021-01-10 23:50:19 -0500

    display High score, clear high score and initiaz

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # coder-interview-quiz
 Quiz test
+
+URL for the app: 
+Files: 
+Script.js
+Index.html
+Style.css
+
+Completed Work(MVP1):
+Screen 1: 
+Shows the user the instructions and the start button to start the quizz. 
+Screen 2: 
+ 
+Once the user starts the game. 
+Questions are asked. A total 2mins of time is given and the answer buttons are populated
+If the user answers correctly/incorrectly the user is shown the results.
+Once the user has gone through all the questions we go to step 3
+Screen 3: 
+User is shown the scores for this round 
+And is given the option to enter their initials in order to save the score. 
+ 
+Screen 4:
+ 
+Lastly the user is given the past scores and the options to clear scores or restart the quiz. 
+
+
+Backlog of Requirements:
+1.	Html and css of the following screen. 
+a.	Title
+b.	paragraph
+c.	Start button
+d.	Header with “view high score” and “timer = 0”
+ 
+
+2.	HTML, CSS and JS
+a.	Timer with amount of time
+b.	Questions are shuffled when user starts the quiz
+c.	Timer subtracts “10” seconds when answer is incorrect
+d.	Timer starts when first question is given (THEN a timer starts and I am presented with a question)
+e.	10 questions in the array with answers in object. 
+f.	User input to answer question with “click”
+g.	Once user answers with “click” the result is logged and user feedback “correct” or “incorrect” is provided. 
+h.	Next question is provided to user
+i.	Test ends when questions are complete or timer reaches 0
+
+3.	HTML, CSS and JS
+a.	Screen appears with final score
+b.	Provides user option to input their initial and submit
+c.	The initial has to be recorded in an array with the score
+d.	Time is shown for each try
+
+
+
+4.	HTML, CSS and JS
+a.	Screen appears with high final score, initial and time taken
+b.	Provides user option to “clear the high scores” array
+c.	Provides user option to go back and retake the quizz
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,124 +1,197 @@
-/*When start button is clicked=> 
--hide instructions screen    
--start timer function
-- show questions 
--let user select answer
--show correct answer
-log result for user.
-for each incorrect answer -10sec
-finish loop when wither timer=0 or questions =0
-give user the score and option to log score
-view high scores (arrange)
-option to restart
-*/
+//HTML elements for DOM manipulation
+var mainBody = document.getElementById("mainPage");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var quizCompleteDiv = document.getElementById("quizComplete");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startButton");
+var startQuizDiv = document.getElementById("firstPage");
+var scoreContainer = document.getElementById("scoreContainer");
+var scorePageDiv = document.getElementById("scorePage");
+var scoreInputName = document.getElementById("initials");
+var scoreDisplayName = document.getElementById("score-initials");
+var endGameBtns = document.getElementById("endBtns");
+var submitScoreBtn = document.getElementById("submitScore");
+var scoreDisplayScore = document.getElementById("score-score");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+
 //Questions copied from https://www.examtiger.com/mcq/css-mcq-on-stylesheet/ this is the question array with answers
-var currentQuestionIndex = 0;
-var score=0;
-const startButton = document.getElementById("start-btn")
-const nextButton= document.getElementById("next-btn")
-const instructionsContainer = document.getElementById("instruction")
-const quesContainer = document.getElementById("quesContainer")
-const questionQuizz =document.getElementById("questions")
-const ansButtonElement =document.getElementById("answer-button")
-const quizTimer = document.getElementById("timeKeeper")
-const highScores = document.getElementById("viewHighScores")
-const correctAnswerShow = document.getElementById("correctAnswer")
-
-var highScoreLog =[];
-startButton.addEventListener("click", hideInstructions);
-highScores.addEventListener("click", showHighScores);
-
-//Step 1 after Start button click
-function hideInstructions (){
-    instructionsContainer.style.display ="none"; //make instructions dissapear
-    quesContainer.style.visibility = "visible";//make question container appear
-    startButton.style.display="none";//hide the start button
-    
-    counTimer();//timer
-    quizzQuestions(); //Show questions
-}
-
-//how to decrease time by 10seconds
-
-function counTimer(){
-    console.log("timer function");
-    var totalTime = 30;
-    var id=setInterval(timerFunction, 1000);
-    function timerFunction(){
-        if(totalTime==0){
-            clearInterval(id);
-            showHighScores();
-        }else {totalTime--;
-            quizTimer.textContent="Time : " +totalTime;
-            }
-        }
-    }
-//timer function ended
-
-//How to show questions and answers
-
 var questions = [
-    {ques:"Can we align a Block element by setting the left and right margins ?", ans1:"Yes we can", ans2:"Not Possible", ans3:"Maybe possible with JQuery", ans4:"Probably", correct:"ans2"},
-    {ques:"If we want define style for an unique element, then which css selector will we use ?", ans1:"Id",ans2:"Text",ans3:"class",ans4:"name", correct:"ans1"},
-    {ques:"If we don't want to allow a floating div to the left side of an element, which css property will we use ?", ans1:"margin",ans2:"clear",ans3:"float",ans4:"padding", correct:"ans2"},
-    {ques:"The default value of position attribute is _________.", ans1:"fixed",ans2:"absolute",ans3:"inherit",ans4:"relative", correct:"ans4"},
-    {ques:"Which CSS property is used to control the text size of an element ?", ans1:"font-style",ans2:"text-size",ans3:"font-size",ans4:"text-style", correct:"ans3"}
-];
-    var questionLenghtIndex=questions.length-1;
+    {q:"If we want define style for an unique element, then which css selector will we use ?",
+    optA:"Id",
+    optB:"text",
+    optC:"class",
+    optD:"name",
+    correctAns:"a"}, 
+    {q:"If we don't want to allow a floating div to the left side of an element, which css property will we use ?",
+    optA:"margin",
+    optB:"clear",
+    optC:"float",
+    optD:"padding",
+    correctAns:"b"}, 
+    {q:"Suppose we want to arragnge five nos. of DIVs so that DIV4 is placed above DIV1. Now, which css property will we use to control the order of stack?",
+    optA:"d-index",
+    optB:"s-index",
+    optC:"x-index",
+    optD:"z-index",
+    correctAns:"d"}, 
+    {q:"Can we align a Block element by setting the left and right margins ?",
+    optA:"Yes we can",
+    optB:"not possible",
+    optC:"maybe",
+    optD:"probably",
+    correctAns:"b"}, 
+    {q:"If we want to wrap a block of text around an image, which css property will we use ?",
+    optA:"wrap",
+    optB:"push",
+    optC:"float",
+    optD:"align",
+    correctAns:"c"}]
 
-    function quizzQuestions(){
+    // Other global variables
+var finalQuestionIndex = questions.length;
+var currentQuestionIndex = 0;
+var timeLeft = 120;
+var timerInterval;
+var score = 0;
+var correct;
+console.log("var declared");
+
+//button to start the quizz
+startQuizButton.addEventListener("click",startQuiz);
+
+// Start Quiz function starts the TimeRanges, hides the start button, & displays the first quiz question.
+function startQuiz(){
+    console.log("start quizz button works");
+    quizCompleteDiv.style.display = "none";
+    startQuizDiv.style.display = "none";
+    generateQuizQuestions();
+
+    //Quiz function starts the TimeRanges.
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        quizTimer.textContent = "Time(in seconds): " + timeLeft;
     
-    if(questionLenghtIndex===currentQuestionIndex){
-        showHighScores();
-    }else {
-        renderQuestionAns(currentQuestionIndex);  
-    }
+        if(timeLeft === 0) {
+          clearInterval(timerInterval);
+          showScore();
+        }
+      }, 1200);
+      mainBody.style.display = "block";
 }
-//END: How to show questions and answers
-
-
-//function to show the question and answer
-
-function renderQuestionAns(currentQuestionIndex){
-    questionQuizz.textContent = "Question: "+(questions[currentQuestionIndex].ques);
-    document.getElementById("ans1").textContent = questions[currentQuestionIndex].ans1;
-    document.getElementById("ans2").textContent = questions[currentQuestionIndex].ans2;
-    document.getElementById("ans3").textContent = questions[currentQuestionIndex].ans3;
-    document.getElementById("ans4").textContent = questions[currentQuestionIndex].ans4;
-  //  correctAnswer();
-}
-
-//END: function to show the question and answer
-
-//Correct Answer checking
-function correctAnswer(answer){
-    correct = questions[currentQuestionIndex].correct;
-    console.log("ques no "+currentQuestionIndex);
-    console.log("the correct answer "+correct);
-    console.log("what i clicked " + answer);
-    if(answer === correct && currentQuestionIndex !== questionLenghtIndex){
-        alert("Correct Answer");
-        score++;
-        currentQuestionIndex++;
-        renderQuestionAns(currentQuestionIndex);
-    } else if (answer !== correct && currentQuestionIndex !== questionLenghtIndex){
-        alert("wrong answer");
-        currentQuestionIndex++;
-        renderQuestionAns(currentQuestionIndex);
-    }else{
-        console.log("You scored " +score+"  out of 5");
-    }
+//the functions looks at the array of questions and asks the user, also knows the correct ans.
+function generateQuizQuestions(){
+    quizCompleteDiv.style.display = "none";
+    if (currentQuestionIndex === finalQuestionIndex){
+        return showScore();
+    } 
+    var currentQuestion = questions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.q + "</p>";
+    buttonA.innerHTML = currentQuestion.optA;
+    buttonB.innerHTML = currentQuestion.optB;
+    buttonC.innerHTML = currentQuestion.optC;
+    buttonD.innerHTML = currentQuestion.optD;
 };
+//Show score
+function showScore(){
+    mainBody.style.display = "none"
+    quizCompleteDiv.style.display = "flex";
+    clearInterval(timerInterval);
+    scoreInputName.value = "";
+    finalScoreEl.innerHTML = "You scored " + score + " out of " + questions.length + " correct!";
+}
 
-//view high scores (arrange)
-function showHighScores(){
-    instructionsContainer.style.display ="none"; //make instructions dissapear
-    quesContainer.style.display = "none";//make question container appear  
-    nextButton.style.display="none";//hide the start button
-    document.getElementById("titleBar").style.display = "none";
-    document.getElementById("currentHighScore").style.visibility="visible";
-    document.getElementById("finalScore").textContent = "You Scored " + score + " out of 5";
-    startButton.textContent = "RESTART";
-    console.log("show high scores");
-    startButton.addEventListener("click", hideInstructions);
+// This function checks the response of the user with the correct answer 
+function correctAnswer(answer){
+    correct = questions[currentQuestionIndex].correctAns;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        //the answer should be on the page
+        alert("That Is Correct!");
+        currentQuestionIndex++;
+        generateQuizQuestions();
+        //display in the results div that the answer is correct.
+    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+        alert("That Is Incorrect.")
+        currentQuestionIndex++;
+        generateQuizQuestions();
+        //display in the results div that the answer is wrong.
+    }else{
+        showScore();
+    }
+}
+
+// This function displays the high scores page
+submitScoreBtn.addEventListener("click", function highscore(){
+    
+    if(scoreInputName.value === "") {
+        alert("Initials cannot be blank");
+        return false;
+    }else{
+        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+        var currentUser = scoreInputName.value.trim();
+        var currentHighscore = {
+            name : currentUser,
+            score : score
+        };
+    
+        quizCompleteDiv.style.display = "none";
+        scoreContainer.style.display = "flex";
+        scorePageDiv.style.display = "block";
+        endGameBtns.style.display = "flex";
+        
+        savedHighscores.push(currentHighscore);
+        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+        generateHighscores();
+
+    }
+    
+});
+// This function clears the list for the high scores and generates a new high score list 
+function generateHighscores(){
+    scoreDisplayName.innerHTML = "";
+    scoreDisplayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i=0; i<highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        scoreDisplayName.appendChild(newNameSpan);
+        scoreDisplayScore.appendChild(newScoreSpan);
+    }
+}
+
+// This function displays the high scores page while hiding every other page
+function showHighscore(){
+    console.log("show high score function");
+    startQuizDiv.style.display = "none"
+    quizCompleteDiv.style.display = "none";
+    highscoreContainer.style.display = "flex";
+    highscoreDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+
+    generateHighscores();
+}
+
+// This function clears the local storage for the score board
+function clearScore(){
+    window.localStorage.clear();
+    scoreDisplayName.textContent = "";
+    scoreDisplayScore.textContent = "";
+}
+
+// This function sets all the variables back to their original values and brings the user buttons to replay of the quiz
+function restart(){
+    scoreContainer.style.display = "none";
+    quizCompleteDiv.style.display = "none";
+    startQuizDiv.style.display = "flex";
+    timeLeft = 120;
+    score = 0;
+    currentQuestionIndex = 0;
 }
