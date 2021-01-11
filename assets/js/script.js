0f792473e7a24c986d37131643c58917b74a51f9
Shamim Imtiaz <km_si@ymail.com>
2021-01-10 19:58:03 -0500

    log result for user, screen for final score, screen for view high score

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 -start timer function
 - show questions 
 -let user select answer
-show correct answer
+-show correct answer
 log result for user.
 for each incorrect answer -10sec
 finish loop when wither timer=0 or questions =0
@@ -22,6 +22,7 @@ const questionQuizz =document.getElementById("questions")
 const ansButtonElement =document.getElementById("answer-button")
 const quizTimer = document.getElementById("timeKeeper")
 const highScores = document.getElementById("viewHighScores")
+const correctAnswerShow = document.getElementById("correctAnswer")
 
 var highScoreLog =[];
 startButton.addEventListener("click", hideInstructions);
@@ -32,15 +33,16 @@ function hideInstructions (){
     instructionsContainer.style.display ="none"; //make instructions dissapear
     quesContainer.style.visibility = "visible";//make question container appear
     startButton.style.display="none";//hide the start button
+    
     counTimer();//timer
     quizzQuestions(); //Show questions
 }
 
-    //how to decrease time by 10seconds
-    //stop timer when button "view high scores" is pressed
+//how to decrease time by 10seconds
+
 function counTimer(){
     console.log("timer function");
-    var totalTime = 15;
+    var totalTime = 30;
     var id=setInterval(timerFunction, 1000);
     function timerFunction(){
         if(totalTime==0){
@@ -54,12 +56,14 @@ function counTimer(){
 //timer function ended
 
 //How to show questions and answers
-//LEFT TO DO : ensure we input the correct questions and answers
 
 var questions = [
-    {ques:"question 1", ans1:"ansA", ans2:"ansB", ans3:"ansC", ans4:"ansD", correct:"ans4"},
-    {ques:"question 2", ans1:"ans2A",ans2:"ans2B",ans3:"ans2C",ans4:"ans2D", correct:"ans2"},
-    {ques:"question 3", ans1:"ans3A",ans2:"ans3B",ans3:"ans3C",ans4:"ans3D", correct:"ans3"}];
+    {ques:"Can we align a Block element by setting the left and right margins ?", ans1:"Yes we can", ans2:"Not Possible", ans3:"Maybe possible with JQuery", ans4:"Probably", correct:"ans2"},
+    {ques:"If we want define style for an unique element, then which css selector will we use ?", ans1:"Id",ans2:"Text",ans3:"class",ans4:"name", correct:"ans1"},
+    {ques:"If we don't want to allow a floating div to the left side of an element, which css property will we use ?", ans1:"margin",ans2:"clear",ans3:"float",ans4:"padding", correct:"ans2"},
+    {ques:"The default value of position attribute is _________.", ans1:"fixed",ans2:"absolute",ans3:"inherit",ans4:"relative", correct:"ans4"},
+    {ques:"Which CSS property is used to control the text size of an element ?", ans1:"font-style",ans2:"text-size",ans3:"font-size",ans4:"text-style", correct:"ans3"}
+];
     var questionLenghtIndex=questions.length-1;
 
     function quizzQuestions(){
@@ -89,8 +93,9 @@ function renderQuestionAns(currentQuestionIndex){
 //Correct Answer checking
 function correctAnswer(answer){
     correct = questions[currentQuestionIndex].correct;
-    console.log("the correct answer"+correct);
-    console.log("what i clicked" + answer);
+    console.log("ques no "+currentQuestionIndex);
+    console.log("the correct answer "+correct);
+    console.log("what i clicked " + answer);
     if(answer === correct && currentQuestionIndex !== questionLenghtIndex){
         alert("Correct Answer");
         score++;
@@ -101,8 +106,7 @@ function correctAnswer(answer){
         currentQuestionIndex++;
         renderQuestionAns(currentQuestionIndex);
     }else{
-        console.log("loop completed");
-        console.log(score);
+        console.log("You scored " +score+"  out of 5");
     }
 };
 
@@ -111,8 +115,10 @@ function showHighScores(){
     instructionsContainer.style.display ="none"; //make instructions dissapear
     quesContainer.style.display = "none";//make question container appear  
     nextButton.style.display="none";//hide the start button
+    document.getElementById("titleBar").style.display = "none";
+    document.getElementById("currentHighScore").style.visibility="visible";
+    document.getElementById("finalScore").textContent = "You Scored " + score + " out of 5";
     startButton.textContent = "RESTART";
     console.log("show high scores");
     startButton.addEventListener("click", hideInstructions);
-    var hgScore = document.createElement("p"); 
 }
